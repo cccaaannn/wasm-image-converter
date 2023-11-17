2797fe391007015ea133fb07f30c617992cfcada
cccaaannn <can.kurt.aa@gmail.com>
2023-11-18 00:25:13 +0300

    alert components

--- src/components/error-alert.tsx
+++ src/components/error-alert.tsx
@@ -7,7 +7,7 @@ interface ErrorAlertProps {
 const ErrorAlert = (props: ErrorAlertProps) => {
     return (
         <div class="border flex items-center rounded-lg p-4 text-sm text-red-800 border-red-300 bg-red-50 dark:bg-gray-800 dark:text-red-400 dark:border-red-800" role="alert">
-            <svg class="flex-shrink-0 inline w-4 h-4 mr-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+            <svg class="self-start flex-shrink-0 inline w-4 h-4 mr-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
                 <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z" />
             </svg>
             <span class="sr-only">Error</span>
