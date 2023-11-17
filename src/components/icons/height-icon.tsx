7961b0d97127b95a5843adee6ad18fde1df738d1
cccaaannn <can.kurt.aa@gmail.com>
2023-11-18 00:24:10 +0300

    svg icons

---
+++ src/components/icons/height-icon.tsx
@@ -0,0 +1,11 @@
+const HeightIcon = () => {
+    return (
+        <svg width="800px" height="800px" viewBox="0 0 48 48" class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor">
+            <path d="M28.6,17.4a1.9,1.9,0,0,0,3-.2,2.1,2.1,0,0,0-.2-2.7l-6-5.9a1.9,1.9,0,0,0-2.8,0l-6,5.9a2.1,2.1,0,0,0-.2,2.7,1.9,1.9,0,0,0,3,.2L22,14.8V33.2l-2.6-2.6a1.9,1.9,0,0,0-3,.2,2.1,2.1,0,0,0,.2,2.7l6,5.9a1.9,1.9,0,0,0,2.8,0l6-5.9a2.1,2.1,0,0,0,.2-2.7,1.9,1.9,0,0,0-3-.2L26,33.2V14.8Z" />
+            <path d="M6,6H42a2,2,0,0,0,0-4H6A2,2,0,0,0,6,6Z" />
+            <path d="M42,42H6a2,2,0,0,0,0,4H42a2,2,0,0,0,0-4Z" />
+        </svg>
+    )
+}
+
+export default HeightIcon;
---
+++ src/components/icons/width-icon.tsx
@@ -0,0 +1,11 @@
+const WidthIcon = () => {
+    return (
+        <svg width="800px" height="800px" viewBox="0 0 48 48" class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor">
+            <path d="M30.6,28.6a1.9,1.9,0,0,0,.2,3,2.1,2.1,0,0,0,2.7-.2l5.9-6a1.9,1.9,0,0,0,0-2.8l-5.9-6a2.1,2.1,0,0,0-2.7-.2,1.9,1.9,0,0,0-.2,3L33.2,22H14.8l2.6-2.6a1.9,1.9,0,0,0-.2-3,2.1,2.1,0,0,0-2.7.2l-5.9,6a1.9,1.9,0,0,0,0,2.8l5.9,6a2.1,2.1,0,0,0,2.7.2,1.9,1.9,0,0,0,.2-3L14.8,26H33.2Z" />
+            <path d="M42,6V42a2,2,0,0,0,4,0V6a2,2,0,0,0-4,0Z" />
+            <path d="M6,42V6A2,2,0,0,0,2,6V42a2,2,0,0,0,4,0Z" />
+        </svg>
+    )
+}
+
+export default WidthIcon;
